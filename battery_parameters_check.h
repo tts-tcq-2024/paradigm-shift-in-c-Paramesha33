14effc2afc6005c687d56decc678f8659fd5f050
Paramesha33 <94032830+Paramesha33@users.noreply.github.com>
2024-11-20 15:06:01 +0530

    Update battery_parameters_check.h

--- battery_parameters_check.h
+++ battery_parameters_check.h
@@ -1,1 +1,9 @@
+#ifndef BATTERY_PARAMETERS_CHECK_H
+#define BATTERY_PARAMETERS_CHECK_H
+#include "common_util.h"
 
+battery_status battery_temperature_check(float value,float min_range,float max_range);
+battery_status battery_soc_check(float value,float min_range,float max_range);
+battery_status battery_charge_rate(float value,float range);
+
+#endif
